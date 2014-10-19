92e3dfff120face76a244b6aa0f73ca284ff9c60
emcallaway <callaway@grinnell.edu>
2014-10-18 22:20:03 -0300

    attempt at getting Heroku to work

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-#config.assets.initialize_on_precompile = false
+
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -13,6 +13,7 @@ end
 
 module HeadStart
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
