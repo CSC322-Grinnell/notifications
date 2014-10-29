0d287a20325572abd232f268ab56150227943e4c
emcallaway <callaway@grinnell.edu>
2014-10-29 01:23:04 -0300

    change account

--- app/controllers/send_sms.rb
+++ app/controllers/send_sms.rb
@@ -1,10 +1,10 @@
 require 'rubygems'
 require 'twilio-ruby'	
 	# Get your Account Sid and Auth Token from twilio.com/user/account
-account_sid = 'AC2f8678b32a948acab39c0d426a966d1c'
-auth_token = '2162328a31a28e73b4c229e116a4250d'
+account_sid = 'ACcb18011c64c77f7b2d26da071b440465'
+auth_token = '95b84b123cf2ab58b04e9036b5448163'
 @client = Twilio::REST::Client.new account_sid, auth_token
-message = @client.account.messages.create(:body => "Jenny please?! I love you <3",
-:to => "+19018266352", # Replace with your phone number
-:from => "+19015319781") # Replace with your Twilio number
+message = @client.account.messages.create(:body => "Jenny please?! I love you. I just love my cat more. <3",
+:to => "+12532363623", # Replace with your phone number
+:from => "+16412438092") # Replace with your Twilio number
 puts message.sid
