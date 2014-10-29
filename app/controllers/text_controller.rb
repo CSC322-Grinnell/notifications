79fb232dcd84ccaf2aa8f3b0643f54d40e1f9cf5
emcallaway <callaway@grinnell.edu>
2014-10-28 22:38:39 -0300

    change twilio account

--- app/controllers/text_controller.rb
+++ app/controllers/text_controller.rb
@@ -17,8 +17,8 @@ def create
 	else
 		flash[:notice] = "Message sent successfully."
 	end
-
-    redirect_to :text => :create
+	render('index')
+   # redirect_to :text => :create
 end
 
 def send_text(number)
@@ -26,12 +26,12 @@ def send_text(number)
    		number = '+1' + number
      	#render :text => "Text to #{number} was succesfully created."
     	#flash[:notice] = "Text to #{number} was succesfully created."
-    	account_sid = 'AC2f8678b32a948acab39c0d426a966d1c'
-		auth_token = '2162328a31a28e73b4c229e116a4250d'
+    	account_sid = 'ACcb18011c64c77f7b2d26da071b440465'#'AC2f8678b32a948acab39c0d426a966d1c'
+		auth_token = '95b84b123cf2ab58b04e9036b5448163'#'2162328a31a28e73b4c229e116a4250d'
 		@client = Twilio::REST::Client.new account_sid, auth_token
 		message = @client.account.messages.create(:body => params[:message],
 		:to => number,
-		:from => "+19015319781") # Replace with your Twilio number
+		:from => "+16412438092")#{}"+19015319781") # Replace with your Twilio number
 		puts message.sid
 	else
 		@invalid_num << number
