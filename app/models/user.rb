230c770f2d9d91abcb34b267ef8cad2cae96cb19
emcallaway <callaway@grinnell.edu>
2014-11-14 23:10:46 -0600

    add authlogic (constraint exception error)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-    attr_accessible :firstName, :lastName, :phone, :email, :type
+attr_accessible :name, :email, :password, :password_confirmation
+  acts_as_authentic do |c|
+    c.login_field = 'email'
+
+  end # block optional
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,8 +20,21 @@ HeadStart::Application.routes.draw do
   #   resources :products
   #root to: 'text#sendtext'
   #resources :text, :user
-resources :text
-root :to => redirect('/text')
+  #config/routes.rb
+  resources :user_sessions
+
+  match 'login' => "user_sessions#new",      :as => :login
+  match 'logout' => "user_sessions#destroy", :as => :logout
+
+  resources :users  # give us our some normal resource routes for users
+  resource :user, :as => 'account'  # a convenience route
+
+  match 'signup' => 'users#new', :as => :signup
+
+  root :to => 'users#new'
+
+  resources :text
+#root :to => redirect('/text')
   # Sample resource route with options:
   #   resources :products do
   #     member do
