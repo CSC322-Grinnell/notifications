f94b2464fafca06f9da7804cdcd61c9fd7401bf7
emcallaway <callaway@grinnell.edu>
2015-04-16 14:01:35 -0500

    Update 20150312200704_add_can_text_to_student.rb

--- db/migrate/20150312200704_add_can_text_to_student.rb
+++ db/migrate/20150312200704_add_can_text_to_student.rb
@@ -1,5 +1,5 @@
 class AddCanTextToStudent < ActiveRecord::Migration
 	def change
-		add_column :students, :Can_Text, :boolean, :default => true
+		add_column :students, :can_text, :boolean, :default => true
 	end
 end
