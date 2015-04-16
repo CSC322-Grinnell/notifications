bf2cba099c62ebbc5b542f677d1e53c760d6f12e
Lee Gatchell <leegatchell@gmail.com>
2015-04-16 15:28:17 -0500

    Update application_controller.rb
    Pretty sure we don't need the stub anymore...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,10 +14,6 @@ class ApplicationController < ActionController::Base
     @current_user = current_user_session && current_user_session.user
   end
 
-  def admin?
-    true
-  end
-
   def require_user
     logger.debug 'ApplicationController::require_user'
     unless current_user
