4585c40ed0a5fa457cab47f414a4e605cd6451bf
Emma Liddell <emmaliddell@users.noreply.github.com>
2015-11-05 15:31:09 -0600

    Removed previously added column "classroom" from students because we've decided to have a relational table instead

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20151105210515) do
+ActiveRecord::Schema.define(:version => 20151105213018) do
 
   create_table "classrooms", :force => true do |t|
     t.string   "name"
