5f05fdc9b0ee84b4b6363ddd8c7f006de887f8bc
Alex Mitchell <alex@alexthemitchell.com>
2016-03-02 19:22:22 -0600

    Add send and fetch methods for messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,21 @@
 class Message < ActiveRecord::Base
-   attr_accessible :contents, :user
-   has_many :student
+  attr_accessible :contents, :user
+  has_many :student
+
+  # Sends message to all recipients
+  def send()
+    receipts = Reciept.find_by_Message(self)
+    receipts.each do |receipt|
+      receipt.send()
+    end
+  end
+
+  # Fetches message with given ID, or all messages if no parameters given.
+  def self.fetch(id=nil)
+    if id == nil
+      return Message.all
+    else
+      return Message.find_by_id(id)
+    end
+  end
 end
--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -1,10 +1,13 @@
 class Receipt < ActiveRecord::Base
   attr_accessible :message, :student
-
+  # TODO: Add message status enumerated type
+  #  Unsent/Queued/Sent/Delivered/Errored
   belongs_to :message
   belongs_to :student
 
+  # Send message to student
   def send()
+    # TODO: Check status and only send if not already sent
     account_sid = 'ACc3ff9be899397461c075ffcf9e70f35a'
     auth_token = '48f209948887f585f820760a89915194'
     @client = Twilio::REST::Client.new account_sid, auth_token
