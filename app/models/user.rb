46c7f10354356ca25ec2b4db773c9cfa7e4e752e
Alex Mitchell <alex@alexthemitchell.com>
2016-03-17 14:17:59 -0500

    Fix text sending center

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,22 @@
 class User < ActiveRecord::Base
-
-attr_accessible :login, :email, :persistence_token, :name, :password, :password_confirmation,
-:crypted_password, :admin, :classroom_ids
-#attr_accessor :password, :password_confirmation, :crypted_password
+  attr_accessible :login, :email, :persistence_token, :name, :password, :password_confirmation,
+  :crypted_password, :admin, :classroom_ids
+  #attr_accessor :password, :password_confirmation, :crypted_password
  acts_as_authentic do |c|
     c.login_field = :email
 
   end # block optional
 
-validates_inclusion_of :admin, :in => [true, false]
+  validates_inclusion_of :admin, :in => [true, false]
 
-has_and_belongs_to_many :classrooms
+  has_and_belongs_to_many :classrooms
+
+  def getClassrooms
+    if self.admin == true
+      return Classroom.all
+    else
+      return self.classrooms
+    end
+  end
+
 end
