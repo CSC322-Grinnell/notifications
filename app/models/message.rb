52f414df0aa03f6903a1d1660e142387580f6064
Alex Mitchell <alex@alexthemitchell.com>
2016-04-14 13:39:04 -0500

    Fix message sending features

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,11 +3,16 @@ class Message < ActiveRecord::Base
   has_many :receipts
   attr_accessible :contents, :user
 
+
+
   # Sends message to all recipients
   def distribute()
+    account_sid = 'ACf91b9f14d8e436a70de5dc8c37a82baf'
+    auth_token = 'f2a3429001ecef51f36940d514eec0dd'
+    client = Twilio::REST::Client.new account_sid, auth_token
     receipts = Receipt.find_all_by_message_id(self)
     receipts.each do |receipt|
-      receipt.distribute()
+      receipt.distribute(client)
     end
   end
 
