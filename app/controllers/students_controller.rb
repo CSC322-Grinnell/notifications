1b84086590fcb031736491538866b299ce5aa51a
edgerton <edgerton@grinnell.edu>
2016-05-10 15:20:00 -0500

    fixed student edit page

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -27,6 +27,11 @@ class StudentsController < ApplicationController
     end
   end
 
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
   def update
     @student = Student.find(params[:id])
 
