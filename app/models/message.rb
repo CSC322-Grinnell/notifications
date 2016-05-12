66de9be3bcebebd838b20538c3db413fab3b6e2c
edgerton <edgerton@grinnell.edu>
2016-05-12 12:39:56 -0500

    adding real credentials

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,8 +7,10 @@ class Message < ActiveRecord::Base
 
   # Sends message to all recipients
   def distribute()
-    account_sid = 'ACa9e2c26aa9c541562abfa60f22c17895'
-    auth_token = '11d34276495a1f8acfb15958672aff55'
+    #account_sid = 'ACa9e2c26aa9c541562abfa60f22c17895'
+    #auth_token = '11d34276495a1f8acfb15958672aff55'
+    account_sid = 'SKa9949dade58a54485cb618099ed15bdb'
+    auth_token = 'XiG07bySRatOqGLd92E1Tf5MpyUHytDx'
     client = Twilio::REST::Client.new account_sid, auth_token
     receipts = Receipt.find_all_by_message_id(self)
     receipts.each do |receipt|
