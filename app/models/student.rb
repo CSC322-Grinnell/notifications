01fe937ed13f6a2ecd7ea86d6bb1d620b9b285c1
uzonwike <uzonwike@gmail.com>
2016-09-26 19:18:01 +0000

    Fixed phone number edits

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,7 +3,7 @@ class Student < ActiveRecord::Base
 
   validates_presence_of :Student_Name, :message => "can't be blank"
   validates_presence_of :Parent_Name, :message => "can't be blank"
-  validates_format_of :Phone_Number, :with =>/(\d{10})|(\(\d{3}\) \d{3}-\d{4})|(\d{3}-\d{3}-\d{4})/, :allow_blank => true, :message => "only allows phoney numbers"
+  validates_format_of :Phone_Number, :with =>/(\d{10})|(\(\d{3}\)-\d{3}-\d{4})|(\d{3}-\d{3}-\d{4})/, :allow_blank => true, :message => "only allows phoney numbers"
   ##validates_length_of :Phone_Number, :is => 10, :allow_blank => true, :message => "#{phone_num} must be 10 digits"
   validates_format_of :Email, :with =>/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i ,:allow_blank => true, :message => 'format is not valid'
   validates_inclusion_of :can_text, :in => [true, false]
