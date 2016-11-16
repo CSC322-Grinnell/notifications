9a45999daec11928d60def706b97974a27463dec
Matt Guthrie <guthriem@grinnell.edu>
2016-11-16 20:28:17 +0000

    fix history page

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -13,6 +13,7 @@ class Receipt < ActiveRecord::Base
     # TODO: Check status and only send if not already sent
     student.contact_ids.each do |contact_id|
       contact_number = Contact.find_by_id(contact_id).Phone_Number
+      puts contact_number
       client.account.messages.create(
         #:from => '+15005550006',
         :from => '+16412438808',
