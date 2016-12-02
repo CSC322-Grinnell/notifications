535d094037f695ba6c32369058bc1a514fee53c8
Niko Takayesu <takayesu17@grinnell.edu>
2016-12-02 20:50:38 +0000

    Texting more than one classroom supported

--- app/controllers/text_controller.rb
+++ app/controllers/text_controller.rb
@@ -10,14 +10,32 @@ class TextController < ApplicationController
     puts params
     text = params[:message]
     student_ids = params[:student].values
-
+    puts "HEYHEYEHYEHEYEH"
+    puts student_ids
     message = Message.create(contents: text, user: @current_user)
 
     student_ids.each do |id|
       student = Student.find_by_id(id)
       student.contacts.each do |contact|
-        Receipt.create(message: message, contact: contact)
+        @existing_receipts = Receipt.find_all_by_message_id(message.id)
+        if @existing_receipts.length == 0
+          Receipt.create(message: message, contact: contact)
+        else
+          @AlreadyExists = false
+          @existing_receipts.each do |old_receipt|
+            if old_receipt.contact == contact 
+                @AlreadyExists = true
+            end
+        end
+        if @AlreadyExists == false
+            Receipt.create(message: message, contact: contact)
+        end
       end
+    end
+      
+      
+      
+      
     end
     message.distribute()
     redirect_to '/history'
