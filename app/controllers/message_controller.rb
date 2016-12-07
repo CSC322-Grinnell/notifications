dd407cdb47a0dd7c736a0eff3c3ced4fa7d2b13f
Matt Guthrie <guthriem@grinnell.edu>
2016-12-07 15:34:29 +0000

    Fixed twilio response

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -8,16 +8,16 @@ class MessageController < ApplicationController
         @Users = User.all
         if ENV_CONFIG['TWILIO_SID'] == @AccountID
             @Users.each do |user|
-                if "+1" + user.phone_number == @sender 
+                if "+1" + user.phone_number == @sender #checks if valid user
                     validUser = true
                     outgoing_message = Message.create(contents: @incomingmsg, user: user)
                     user.classroom_ids.each do |classroom_id|
                         Classroom.find_by_id(classroom_id).student_ids.each do |student_id|
                             student = Student.find_by_id(student_id)
                             student.contacts.each do |contact|
-                                @existing_receipts = Receipt.find_all_by_message_id(outgoing_message.id)
+                                @existing_receipts = Receipt.find_all_by_message_id(outgoing_message.id) #creates receipt for contacts
                                 if @existing_receipts.length == 0
-                                    Receipt.create(message: outgoing_message, contact: contact)
+                                    Receipt.create(message: outgoing_message, contact: contact) #only creates one receipt per contact
                                 else
                                     @AlreadyExists = false
                                     @existing_receipts.each do |old_receipt|
@@ -36,7 +36,7 @@ class MessageController < ApplicationController
                 end
             end
         end # if ...
-        if (validUser)
+        if (validUser) #responds to the incoming number based on if they are a valid user or not
             render formats: :xml
         else
             render :file => "message/altmsg.xml.erb", formats: :xml
